2e7c248d4191ff265de8d65f264c5c9f5bd6060e
willfull <willfull.git.box@yandex.ua>
2020-06-18 18:57:14 +0300

    Add - initial files for Day component

---
+++ public/components/calendar/day/Day.js
@@ -0,0 +1,15 @@
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import ReactDOM from 'react-dom';
+import style    from './style.css';
+
+function Day(){
+  return (
+    <div className={style.day}>
+    </div>
+  );
+}
+
+export default Day;
