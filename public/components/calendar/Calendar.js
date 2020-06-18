37967c7c404606fb76e8104e839ffa8906e711b7
willfull <willfull.git.box@yandex.ua>
2020-06-18 15:35:48 +0300

    Add - initial files for component

---
+++ public/components/calendar/Calendar.js
@@ -0,0 +1,16 @@
+import React    from 'react';
+import ReactDOM from 'react-dom';
+import style    from './style.css';
+
+// Log
+console.log(style);
+
+function Calendar(){
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Calendar;
