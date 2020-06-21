53744cd611c2e86f44f50d762ae03582140ee85d
willfull <willfull.git.box@yandex.ua>
2020-06-21 13:51:10 +0300

    Add - cmp Stats - base styles

--- public/components/stats/Stats.js
+++ public/components/stats/Stats.js
@@ -2,11 +2,54 @@ import React, {
   useState,
   useEffect
 } from 'react';
+import classes from './stats.css';
 
 function Stats(){
+  // State..
+
   return (
-    <div>
-      
+    <div className={classes.cnt}>
+      <ul className={classes.col}>
+        <li className={classes.row}>
+          Start/End:
+        </li>
+        <li className={classes.row}>
+          Time period:
+        </li>
+        <li className={classes.row}>
+          Work days:
+        </li>
+      </ul>
+      <ul className={classes.col}>
+        <li className={classes.row}>
+          Chill Days:
+        </li>
+        <li className={classes.row}>
+          Light Days:
+        </li>
+        <li className={classes.row}>
+          Normal Days:
+        </li>
+      </ul>
+      <ul className={classes.col}>
+        <li className={classes.row}>
+          Hard Days:
+        </li>
+        <li className={classes.row}>
+          Hardcore Days:
+        </li>
+      </ul>
+      <ul className={classes.col}>
+        <li className={classes.row}>
+          Author:
+        </li>
+        <li className={classes.row}>
+          Date:
+        </li>
+        <li className={classes.row}>
+          Likes:
+        </li>
+      </ul>
     </div>
   );
 }
