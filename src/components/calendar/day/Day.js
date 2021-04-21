fb8e2df30911c5fc928bdaac591d1d0ed588220d
willfull-git <willfull.git.box@yandex.ua>
2021-04-21 16:05:53 +0300

    Add - Calendar selection functionality

--- src/components/calendar/day/Day.js
+++ src/components/calendar/day/Day.js
@@ -21,7 +21,6 @@ import {
 } from '../../../context';
 
 import {
-  performEditorCommands,
   getEditorCommandsModel
 } from '../../../utils/utilsEditorCommands';
 
@@ -35,7 +34,8 @@ function Day(props){
   const {yearDays}                          = useContext(YearDaysContext);
 
   const [commitLevel, setCommitLevel] = useState(0);
-  const [ownEditorCmdFlags, setOwntEditorCmdFlags] = useState(getEditorCommandsModel());
+  const [selected, setSelected]       = useState(false);
+  const [ownEditorCmdFlags, setOwnEditorCmdFlags] = useState(getEditorCommandsModel());
 
   const dateFormatted  = format(props.date, 'yyyy.MM.dd');
   const editorCmdFlags = getEditorCommandsModel();
@@ -49,12 +49,112 @@ function Day(props){
   // | Perform Editor Commands
   // |----------
   useEffect(()=>{
-    performEditorCommands(
-      editorCmdFlags, ownEditorCmdFlags,
-      editorCommands, setOwntEditorCmdFlags,
-      setCommitLevel, setSketch,
-      props.date, dateFormatted, commitLevel, sketch
-    );
+    // // |--- Validation
+    if(JSON.stringify(editorCmdFlags) === JSON.stringify(ownEditorCmdFlags)) return;
+
+    // // |--- Find out Active Command
+    let activeCommand;
+
+    for(let command in editorCmdFlags){
+      if(
+        editorCmdFlags[command].flag
+        !==
+        ownEditorCmdFlags[command].flag
+      ){
+        activeCommand = command;
+
+        break;
+      }
+    }
+
+    // // |--- Perform Active Command
+    if( activeCommand ){
+      let editorCmdFlagsCopy = Object.assign({}, editorCmdFlags);
+
+      switch(activeCommand){
+        case 'clearAll':
+          setOwnEditorCmdFlags(editorCmdFlagsCopy);
+          setCommitLevel(0);
+          setSketch([]);
+
+          break;
+        case 'addRange':
+          setOwnEditorCmdFlags(editorCmdFlagsCopy);
+
+          editorCommands.addRange.range.forEach((v, i)=>{
+            if(i===0) return;
+
+            if( cmpDate.getTime()==v.getTime() ){
+              let
+                sketchCopy = sketch.slice(),
+                level  = (cmpCommitLevel+5+1)%5,
+                commit = {
+                  id: cmpDateFormatted,
+                  date: cmpDate,
+                  level: level
+                };
+
+              setCommitLevel(level);
+              sketchCopy.push(commit);
+
+              setSketch(sketchCopy);
+            } 
+          });
+
+          break;
+        case 'select':
+          setOwnEditorCmdFlags(editorCmdFlagsCopy);
+
+          let
+            dayPos    = refBox.current.getBoundingClientRect(),
+            selectPos = editorCommands.select.overlayPos;
+
+          if(
+            dayPos.left+dayPos.width < selectPos.left+selectPos.width
+            &&
+            dayPos.left > selectPos.left
+            &&
+            dayPos.top+dayPos.height < selectPos.top+selectPos.height 
+            &&
+            dayPos.top > selectPos.top
+          ){
+            let copyEditorCommands = Object.assign({}, editorCommands);
+
+            switch(editorCommands.select.mode){
+              case 'new':
+                copyEditorCommands.select.selectedDays.push({date: dateFormatted});
+                setSelected(true);
+                break;
+              case 'add':
+                copyEditorCommands.select.selectedDays.push({date: dateFormatted});
+                setSelected(true);
+                break;
+              case 'remove':
+                copyEditorCommands.select.selectedDays.forEach((v, i)=>{
+                  if(v.date===dateFormatted){
+                    copyEditorCommands.select.selectedDays.splice(i, 1);
+                  }
+                });
+                setSelected(false);
+                break;
+            }
+
+            setEditorCommands(copyEditorCommands);
+          } else if(selected && editorCommands.select.mode==='new') {
+            setSelected(false);
+          }
+
+          break;
+        case 'unselect':
+          // console.log('--| [unselect]');
+
+          if(selected){
+            setSelected(false);
+          }
+          break;
+        default:
+      }
+    }
   }, [editorCommands]);
 
   // | Handler - Click
@@ -92,7 +192,11 @@ function Day(props){
   // -----
   return (
     <div
-      className={classes.tile+' '+'commit-'+commitLevel}
+      className={
+        classes.tile+' '+'commit-'+commitLevel
+        + ' ' +
+        (selected? classes.selected: '')
+      }
       title={dateFormatted}
       onClick={handleClick}
       ref={refBox}
